b295f45e52d6ba996d1b0e9b1abe3f62daf99989
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-09-05 17:30:00 +0300

    play testing practice
    add some play mode testing exemples

---
+++ Assets/UnitTesting/Humble Object and Substitute Practice/Scripts/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PracticeProject.UnitTesting.PMTestingPractice
+{
+    public class Circle
+    {
+        private int _spawnRadius;
+
+        public Circle(int radius)
+        {
+            _spawnRadius = radius;
+        }
+
+        public Vector3 GetPositionOnCircleBoundaries(int degrees)
+        {
+            return new Vector3(CalculateXCoord(degrees), 0f, CalculateYCoord(degrees));
+        }
+
+        private float CalculateYCoord(int degrees)
+        {
+            var y = _spawnRadius * Mathf.Sin(degrees * Mathf.Deg2Rad);
+            if (Mathf.Abs(y) < 0.01f) y = 0;
+            return y;
+        }
+
+        private float CalculateXCoord(int degrees)
+        {
+            var x = _spawnRadius * Mathf.Cos(degrees * Mathf.Deg2Rad);
+            if (Mathf.Abs(x) < 0.01f) x = 0;
+            return x;
+        }
+    }
+
+}
